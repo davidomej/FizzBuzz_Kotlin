814edf81b8d5de1e2cb9beaab829f9a01d937008
david <davidomej@hotmail.com>
2022-04-06 19:40:33 +0100

    Finish

--- src/Main/FizzBuzz.kt
+++ src/Main/FizzBuzz.kt
@@ -1,8 +1,15 @@
 package Main
 
 class FizzBuzz {
-    fun play(i: Int): Int {
-        return i;
+    fun play(i: Int): String {
+        if(i%5==0 && i%3==0){
+            return "FizzBuzz";
+        }else if(i%5==0) {
+            return "Buzz";
+        }else if(i%3==0){
+            return "Fizz";
+        }
+        return ""+i;
     }
 
 }
--- src/Test/FizzBuzzShould.kt
+++ src/Test/FizzBuzzShould.kt
@@ -9,7 +9,10 @@ class FizzBuzzShould {
     @Test
     fun test_library(){
         val fb = FizzBuzz();
-        assertEquals(2,fb.play(2));
+        assertEquals<String>("2",fb.play(2));
+        assertEquals<String>("Fizz", fb.play(3));
+        assertEquals<String>("Buzz", fb.play(5));
+        assertEquals<String>("FizzBuzz", fb.play(15));
     }
 
 }
